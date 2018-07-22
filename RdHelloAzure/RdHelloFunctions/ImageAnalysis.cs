d1c522fe184cf35521d460d614e15273acb4fcc0
Rama Dasari <12179940+dasariramacharan@users.noreply.github.com>
2018-07-22 23:03:39 +0800

    integration with Congnitive services to recogniese face

--- RdHelloAzure/RdHelloFunctions/ImageAnalysis.cs
+++ RdHelloAzure/RdHelloFunctions/ImageAnalysis.cs
@@ -1,15 +1,18 @@
 using System;
 using System.IO;
+using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.ProjectOxford.Face.Contract;
+using Microsoft.ProjectOxford.Face;
 
 namespace RdHelloFunctions
 {
     public static class ImageAnalysis
     {
         [FunctionName("ImageAnalysis")]
-        public static void Run([BlobTrigger("images/{name}", Connection = "rdhellostorage")]
+        public static async void Run([BlobTrigger("images/{name}", Connection = "rdhellostorage")]
                                 CloudBlockBlob blob, string name, TraceWriter log)
         {
             log.Info($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {blob.Properties.Length} Bytes");
@@ -20,6 +23,19 @@ namespace RdHelloFunctions
             log.Info($"Blob url is {url}");
 
             //TODO: test running this code with test data 
+
+            var faces = await GetAnalysisAsync(url);
+
+        }
+
+        public static async Task<Face[]> GetAnalysisAsync(string url)
+        {
+            var client = new FaceServiceClient("83e27e8f9a4a47bda12612275fa77d66",  //TODO:Secure key
+                                "https://southeastasia.api.cognitive.microsoft.com/face/v1.0");
+            var types = new []{ FaceAttributeType.Emotion };
+            var result = await client.DetectAsync(url, false, false, types);
+            return result;
+
         }
 
         public static string GetSas(CloudBlockBlob blob)
